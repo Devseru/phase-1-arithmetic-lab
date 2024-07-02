197f99302cd6afcd4db992e46a3e8c5a30814c6b
Devseru <serushikoli65@gmail.com>
2024-07-02 15:43:01 +0300

    completing arithmeting lab

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
+let num1 = 2;  
+let num2 = 31; 
 
-// Write your code here
+let multiply = num1 * num2; 
+
+console.log(multiply); 
+
+let random = Math.floor(Math.random() * 100) + 1;
+
+console.log(random); 
+
+let num3 = 14; // Example value for num3
+let num4 = 5;  // Example value for num4
+
+let mod = num3 % num4; // mod should be 4
+
+console.log(mod); // This should output 4
+
+let numbers = [1, 3, 5, 7, 20, 2, 4, 6, 8]; // Example set of numbers
+let max = Math.max(...numbers); // Spread the array to pass its elements as individual arguments to Math.max
+
+console.log(max); // This should output 20
+
+
+
